5bed2281a3ecb1a4a9cf11fd95af6d0250a73b7c
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-01-14 14:38:04 +0100

    move incoming to gateway.rs

--- src/api/model/mod.rs
+++ src/api/model/mod.rs
@@ -1,30 +1,3 @@
 pub mod resume;
 pub mod play;
-
-use twilight_model::gateway::payload::incoming::{VoiceServerUpdate, VoiceStateUpdate};
-use twilight_model::voice::VoiceState;
-use twilight_model::gateway::event::Event;
-
-#[derive(serde::Deserialize, Debug)]
-#[serde(tag = "op", content = "data")]
-#[serde(rename_all = "snake_case")]
-pub enum Incoming {
-    UpdateVoiceServer(VoiceServerUpdate),
-    UpdateVoiceState(VoiceState),
-}
-
-impl Incoming {
-    pub fn is_voice_event(&self) -> bool {
-        matches!(self, Incoming::UpdateVoiceState(_)) || matches!(self, Incoming::UpdateVoiceServer(_))
-    }
-}
-
-impl Into<Event> for Incoming {
-    fn into(self) -> Event {
-        match self {
-            Self::UpdateVoiceServer(update) => Event::VoiceServerUpdate(update),
-            Self::UpdateVoiceState(state) => Event::VoiceStateUpdate(Box::new(VoiceStateUpdate(state))),
-            _ => unreachable!()
-        }
-    }
-}
+pub mod gateway;
