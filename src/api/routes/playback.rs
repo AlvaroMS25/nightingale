73b0a9c29fccdf7335b080b16061a343bc81d899
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-01-14 23:37:10 +0100

    insert metadata

--- src/api/routes/playback.rs
+++ src/api/routes/playback.rs
@@ -6,13 +6,14 @@ use axum::http::StatusCode;
 use axum::Json;
 use axum::response::{IntoResponse, Response};
 use serde::Deserialize;
-use songbird::input::{Input, YoutubeDl};
+use songbird::input::{AuxMetadata, Compose, Input, YoutubeDl};
 use tracing::info;
 use uuid::Uuid;
 use crate::api::extractors::call::CallExtractor;
 use crate::api::extractors::session::SessionExtractor;
 use crate::api::model::play::{PlayOptions, PlaySource};
 use crate::api::state::State;
+use crate::playback::metadata::TrackMetadata;
 
 const NOT_CONNECTED: &str = r#"{"message": "Not connected to voice"}"#;
 
@@ -29,9 +30,30 @@ pub async fn play(
     Json(options): Json<PlayOptions>
 ) -> impl IntoResponse {
     info!("Received play request");
-    let source: Input = match options.source {
-        PlaySource::Bytes(bytes) => bytes.into(),
-        PlaySource::Link(link) => YoutubeDl::new(state.http.clone(), link).into()
+    let (source, metadata): (Input, _) = match options.source {
+        PlaySource::Bytes(bytes) => {
+            (bytes.into(), TrackMetadata {
+                metadata: AuxMetadata::default(),
+                guild: query.guild_id.get()
+            })
+        },
+        PlaySource::Link(link) => {
+            let mut ytdl = YoutubeDl::new(state.http.clone(), link);
+
+            let metadata = match ytdl.aux_metadata().await {
+                Ok(m) => m,
+                Err(e) => {
+                    return Response::builder()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .body(Body::from(format!(r#"{{"message": "{e}"}}"#)))
+                        .unwrap();
+                }
+            };
+            (ytdl.into(), TrackMetadata {
+                metadata,
+                guild: query.guild_id.get()
+            })
+        }
     };
 
     let mut lock = session.write().await;
@@ -44,6 +66,8 @@ pub async fn play(
     };
 
     let handle = call.lock().await.enqueue_input(source).await;
+
+    handle.typemap().write().await.insert::<TrackMetadata>(metadata);
 
     lock.playback.queue.for_guild(query.guild_id.get()).push(handle);
 
