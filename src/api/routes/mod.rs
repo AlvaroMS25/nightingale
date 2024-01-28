cf116b078ca430cc77fb2a5d73958c73ac4a52df
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-01-28 02:25:16 +0100

    change request methods

--- src/api/routes/mod.rs
+++ src/api/routes/mod.rs
@@ -1,19 +1,21 @@
 use axum::Router;
 use axum::extract::DefaultBodyLimit;
-use axum::routing::{patch, put};
+use axum::routing::{delete, get, patch, post, put};
 use crate::api::state::State;
 
 mod playback;
 mod gateway;
+mod prometheus;
 
 pub fn get_router() -> Router<State> {
     Router::new()
         .route("/connect", put(gateway::connect))
-        .route("/disconnect", put(gateway::disconnect))
+        .route("/disconnect", delete(gateway::disconnect))
         .nest("/playback", Router::new()
-            .route("/play", put(playback::play).layer(DefaultBodyLimit::disable()))
-            .route("/pause", put(playback::pause))
-            .route("/resume", put(playback::resume))
+            .route("/play", post(playback::play).layer(DefaultBodyLimit::disable()))
+            .route("/pause", patch(playback::pause))
+            .route("/resume", patch(playback::resume))
             .route("/volume/:vol", patch(playback::volume))
         )
+        .route("/prometheus", get(prometheus::prometheus_metrics))
 }
