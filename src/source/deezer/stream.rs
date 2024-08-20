911511312c4ec72dc5b0af3031a7701ffc72dbdc
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-08-20 16:54:04 +0200

    implement buf type and wait for whole blocks on deezer stream

--- src/source/deezer/stream.rs
+++ src/source/deezer/stream.rs
@@ -1,4 +1,6 @@
 use std::io::{ErrorKind, Read, Seek, SeekFrom};
+use std::io::ErrorKind::WouldBlock;
+use std::ops::{Deref, DerefMut};
 use async_trait::async_trait;
 use blowfish::Blowfish;
 use cbc::cipher::{BlockDecryptMut, KeyIvInit};
@@ -25,7 +27,7 @@ impl Compose for DeezerHttpStream {
         Ok(AudioStream {
             input: Box::new(DeezerMediaSource {
                 inner: inner.input,
-                read_buf: [0; 2048],
+                read_buf: Buf::new(),
                 out_buf: Vec::with_capacity(2048),
                 decryptor: Decryptor::new_from_slices(self.key.as_bytes(), &SECRET_IV).unwrap(),
                 current_chunk: 0,
@@ -45,7 +47,7 @@ impl Compose for DeezerHttpStream {
 
 pub struct DeezerMediaSource {
     inner: Box<dyn MediaSource>,
-    read_buf: [u8; 2048],
+    read_buf: Buf<2048>,
     out_buf: Vec<u8>,
     current_chunk: usize,
     decryptor: Decryptor<Blowfish>
@@ -55,7 +57,7 @@ impl DeezerMediaSource {
     fn decrypt_block(&mut self) {
         let dec = self.decryptor
             .clone()
-            .decrypt_padded_mut::<NoPadding>(&mut self.read_buf);
+            .decrypt_padded_mut::<NoPadding>(self.read_buf.inner_mut());
 
         match dec {
             Ok(d) => self.out_buf.extend(d),
@@ -66,13 +68,15 @@ impl DeezerMediaSource {
 
 impl Read for DeezerMediaSource {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let read_bytes = self.inner.read(&mut self.read_buf)?;
+        while !self.read_buf.is_full() && self.read_buf.read_from(&mut self.inner)? > 0 {}
 
-        if self.current_chunk % 3 > 0 || read_bytes < 2048 {
-            self.out_buf.extend(&self.read_buf[0..read_bytes]);
+        if self.current_chunk % 3 > 0 || self.read_buf.len() < 2048 {
+            self.out_buf.extend(&*self.read_buf);
         } else {
             self.decrypt_block();
         }
+
+        self.read_buf.reset();
 
         self.current_chunk += 1;
 
@@ -81,6 +85,7 @@ impl Read for DeezerMediaSource {
         let drain_len = drain.len();
         buf[0..drain_len].copy_from_slice(drain.as_ref());
 
+        //tracing::info!("Read {} bytes", drain_len);
         Ok(drain_len)
     }
 }
@@ -100,3 +105,62 @@ impl MediaSource for DeezerMediaSource {
         self.inner.byte_len()
     }
 }
+
+pub struct Buf<const SIZE: usize> {
+    inner: [u8; SIZE],
+    len: usize
+}
+
+impl<const SIZE: usize> Buf<SIZE> {
+    pub fn new() -> Self {
+        Self {
+            inner: [0; SIZE],
+            len: 0
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn remaining_mut(&mut self) -> &mut [u8] {
+        &mut self.inner[self.len..]
+    }
+
+    pub fn read_from<R: Read>(&mut self, reader: &mut R) -> std::io::Result<usize> {
+        let read_bytes = reader.read(self.remaining_mut())?;
+        self.len += read_bytes;
+
+        Ok(read_bytes)
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len == SIZE
+    }
+
+    pub fn reset(&mut self) {
+        self.len = 0;
+    }
+
+    pub fn inner(&self) -> &[u8] {
+        &self.inner
+    }
+
+    pub fn inner_mut(&mut self) -> &mut [u8] {
+        &mut self.inner
+    }
+}
+
+impl<const SIZE: usize> Deref for Buf<SIZE> {
+    type Target = [u8];
+    fn deref(&self) -> &Self::Target {
+        &self.inner[..self.len]
+    }
+}
+
+impl<const SIZE: usize> DerefMut for Buf<SIZE> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner[..self.len]
+    }
+}
+
